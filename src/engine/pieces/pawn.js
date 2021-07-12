8049be5ab718ba033d70c3c10a15921220bcc6ee
Jack Parkinson <Jack.Parkinson@softwire.com>
2021-07-12 11:24:39 +0100

    Made pawns move or something

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,6 @@
+import Player from '../player';
 import Piece from './piece';
+import Square from '../square';
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +8,14 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let currentPositionOfPawn = board.findPiece(this);
+        if (this.player == Player.BLACK) {
+            const square_below = new Square(currentPositionOfPawn.row -1,currentPositionOfPawn.col)
+            return [square_below]
+        }else if
+            (this.player == Player.WHITE){
+                const square_above = new Square(currentPositionOfPawn.row+1,currentPositionOfPawn.col)
+                return [square_above]
+            }
+        }
     }
-}
